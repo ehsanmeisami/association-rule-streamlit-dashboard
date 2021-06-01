cc26f87bc943caffda5aa1e2d8f4149daef67c9b
Ehsan Meisami <66878542+ehsanmeisami@users.noreply.github.com>
2021-06-01 18:58:09 +0200

    Update association_app.py

--- association_app.py
+++ association_app.py
@@ -2,9 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pathlib
-from apyori import apriori
-# from mlxtend.frequent_patterns import apriori
-# from mlxtend.frequent_patterns import association_rules
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 
 st.write("""
 # Association Rule 
